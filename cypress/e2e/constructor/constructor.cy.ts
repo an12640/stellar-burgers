e6b99f0870e7497a2af54b6647c94c722ed27c27
Anna Petrushkina <annaspetrushkina@gmail.com>
2026-01-18 17:21:15 +0300

    added modal window e2e tests

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,5 +1,8 @@
 const bun = '[data-cy="643d69a5c3f7b9001cfa093c"]';
 const constructor = '[data-cy="constructor"]';
+const modal = '[data-cy="modal"]';
+const modalCloseButton = '[data-cy="modal-close-button"]';
+const modalOverlay = '[data-cy="modal-overlay"]';
 
 beforeEach(() => {
   cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
@@ -13,5 +16,32 @@ describe('Проверяем добавление ингредиентов в к
     it('Добавление булки в конструктор ', function() {
         cy.get(bun).children('button').click();
         cy.get(constructor).should('contain', "Краторная булка N-200igogo");
+    });
+});
+
+describe('Проверяем открытие и закрытие модального окна с описанием ингредиента', function() {
+    it('Открытие модального окна с описанием ингредиента ', function() {
+      cy.get(modal).should('not.exist');  
+      cy.get(bun).find('a').click();
+      cy.get(modal)
+        .should('exist')
+        .should('contain', "Краторная булка N-200igogo");
+      cy.get(modal)
+        .find('img')
+        .should('be.visible')
+    });
+
+    it('Закрытие модального окна по клику на крестик ', function() {
+      cy.get(bun).find('a').click();
+      cy.get(modal).should('exist');
+      cy.get(modalCloseButton).click();
+      cy.get(modal).should('not.exist');
+    });
+
+    it('Закрытие модального окна по клику за областью модального окна ', function() {
+      cy.get(bun).find('a').click();
+      cy.get(modal).should('exist');
+      cy.get(modalOverlay).click({ force: true });
+      cy.get(modal).should('not.exist');
     });
 });
